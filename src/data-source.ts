445a0763cb4ece77c015f5be1c9167f889b4bb24
Andressa Medeiros <andressa.scharaiber@gmail.com>
2025-02-16 21:10:29 -0300

    criado as rotas

--- src/data-source.ts
+++ src/data-source.ts
@@ -9,7 +9,7 @@ export const AppDataSource = new DataSource({
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    synchronize: false,
+    synchronize: true,
     logging: process.env.NODE_ENV === 'development' ? true : false ,
     entities: ["src/entities/*.ts"],
     migrations: ["src/database/migrations/*.ts"],
